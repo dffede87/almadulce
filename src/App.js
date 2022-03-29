5c3765e126e7d6224f477f7a4e78c8bbe98a482a
Federico Gigena <dffede87@gmail.com>
2022-03-28 22:47:48 -0300

    Cambios App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import Icon from '@mui/material/Icon';
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+
 
 export default function App() {
   return (
@@ -8,7 +11,19 @@ export default function App() {
        <Button variant="contained" color="secondary" startIcon={<Icon>delete</Icon>}>
          Delete
        </Button>
-       <Icon color="primary">room</Icon>
+       <Icon color="primary">delete</Icon>
+       <IconButton aria-label="delete">
+        <Icon color="primary">delete</Icon>
+       </IconButton>
+       <Typography variant="h2" color="primary" gutterBottom>
+         Federico Martín Gigena
+       </Typography>
+       <Typography variant="body1" color="initial" align="center" gutterBottom>
+         Lorem ipsum blablabalabla ablablaba ablablabalbala ablablablablaaaba ablababalba ablabalbalbala ablabalbalbalabalbala balbala ablabla abla
+       </Typography>
+       <Typography variant="body1" color="initial" align="center">
+         Lorem ipsum blablabalabla ablablaba ablablabalbala ablablablablaaaba ablababalba ablabalbalbala ablabalbalbalabalbala balbala ablabla abla
+       </Typography>
     </>
   );
 }
