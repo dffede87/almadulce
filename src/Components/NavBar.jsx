ea200727a3e58a61059885de6e10e1126be725af
Federico Gigena <dffede87@gmail.com>
2022-03-29 21:54:58 -0300

    Cambios

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Icon from "./Icon";
+
+export default function NavBar() {
+  return (
+    <>
+       LOGO | Link | Link | Link | <Icon cant={10} />
+    </>
+  );
+}
